b7c3b3233c1fa1355885d7a0bf0497f8922b9c38
Alessandro Lira <al.lira.co@gmail.com>
2019-04-29 16:02:32 -0700

    Configure stdafx.cpp and stdafx.h to house all static imports to exist as a pre-compiled header

--- kanji_kombat/source/stdafx.cpp
+++ kanji_kombat/source/stdafx.cpp
@@ -3,3 +3,5 @@
 //
 
 #include "stdafx.h"
+
+// TODO: reference any additional headers you need in stdafx.h and not in this file
--- kanji_kombat/source/stdafx.h
+++ kanji_kombat/source/stdafx.h
@@ -5,4 +5,21 @@
 #ifndef KANJI_KOMBAT_STDAFX_H
 #define KANJI_KOMBAT_STDAFX_H
 
+#include <stdio.h>
+
+// TODO: reference additional headers your program requires here
+#include <SFML/System.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+
+#include <map>
+#include <iostream>
+#include <cassert>
+#include <list>
+
+#include "Game.h"
+#include "MainMenu.h"
+#include "SplashScreen.h"
+
 #endif //KANJI_KOMBAT_STDAFX_H
